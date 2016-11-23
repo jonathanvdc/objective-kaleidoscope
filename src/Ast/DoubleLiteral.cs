2141f2d82bc91191e50f3fe0d6c507c3473ef808
jonathanvdc <jonathan.vdc@outlook.com>
2016-11-23 23:21:40 +0100

    Create an AST node for double literals

---
+++ src/Ast/DoubleLiteral.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ObjKaleidoscope.Semantics;
+using Flame.Compiler.Expressions;
+
+namespace ObjKaleidoscope.Ast
+{
+    /// <summary>
+    /// An double literal AST node.
+    /// </summary>
+    public class DoubleLiteral : IExpressionNode
+    {
+        public DoubleLiteral(Token Value)
+        {
+            this.Value = Value;
+        }
+
+        /// <summary>
+        /// Gets the value of this double literal, as a token.
+        /// </summary>
+        /// <value>The double literal's value.</value>
+        public Token Value { get; private set; }
+
+        /// <inheritdoc/>
+        public TypeOrValue Analyze(LocalScope Scope)
+        {
+            double result;
+            if (!double.TryParse(Value.Contents, out result))
+            {
+                throw new Exception(
+                    "invalid double-precision floating point literal '" + 
+                    Value.Contents + "'.");
+            }
+
+            // Wrap the double in an expression, and create a 
+            // TypeOrValue instance from that.
+            return new TypeOrValue(new Float64Expression(result));
+        }
+    }
+}
+
