b2fe3a3e33f5dd5080b57ff6e5e459a0e117f395
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-04 22:51:10 +0100

    Define the IdentifierReference type

---
+++ src/Ast/IdentifierReference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ObjKaleidoscope.Semantics;
+using Flame.Compiler.Expressions;
+
+namespace ObjKaleidoscope.Ast
+{
+    /// <summary>
+    /// An AST node that references an identifier. 
+    /// </summary>
+    public sealed class IdentifierReference : IExpressionNode
+    {
+        public IdentifierReference(Token Identifier)
+        {
+            this.Identifier = Identifier;
+        }
+
+        /// <summary>
+        /// Gets the identifier that is referenced, as a token.
+        /// </summary>
+        /// <value>The identifier that is referenced.</value>
+        public Token Identifier { get; private set; }
+
+        /// <inheritdoc/>
+        public TypeOrValue Analyze(ILocalScope Scope)
+        {
+            string name = Identifier.Contents;
+
+            // TODO: handle types, functions and globals
+            return new TypeOrValue(Scope.GetLocal(name));
+        }
+    }
+}
+
