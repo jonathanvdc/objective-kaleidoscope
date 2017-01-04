6a978a5f80204eee9cd1d0ff43c867199f04ba18
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-04 22:41:48 +0100

    Replace LocalScope by ILocalScope

---
+++ src/Semantics/ILocalScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjKaleidoscope.Semantics
+{
+    /// <summary>
+    /// A common interface for local scopes.
+    /// </summary>
+    public interface ILocalScope
+    {
+        /// <summary>
+        /// Gets the local variable with the given name, if
+        /// such a variable exists. Otherwise, returns null.
+        /// </summary>
+        /// <returns>
+        /// The local variable with the given name, if there is such a variable; 
+        /// otherwise, null.</returns>
+        /// <param name="Name">The name of the variable to find.</param>
+        IValue GetLocal(string Name);
+    }
+}
+
---
+++ src/Semantics/LocalDeclScope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjKaleidoscope.Semantics
+{
+    /// <summary>
+    /// A local scope implementation for 
+    /// local variable declarations.
+    /// </summary>
+    public sealed class LocalDeclScope : ILocalScope
+    {
+        public LocalDeclScope(
+            ILocalScope Parent, 
+            string VariableName, IValue Variable)
+        {
+            this.parentScope = Parent;
+            this.variableName = VariableName;
+            this.variable = Variable;
+        }
+
+        // This local scope's parent local scope.
+        private ILocalScope parentScope;
+
+        // The name of the variable defined by this scope.
+        private string variableName;
+
+        // The value of the variable defined by this scope.
+        private IValue variable;
+
+        /// <inheritdoc/>
+        public IValue GetLocal(string Name)
+        {
+            if (variableName == Name)
+                return variable;
+            else
+                return parentScope.GetLocal(Name);
+        }
+    }
+}
+
